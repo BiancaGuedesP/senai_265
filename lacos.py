207db1beb9cd91aa2fad0d650a9585ebd2ef9188
BiancaGuedesP <biancaguedespimentel@gmail.com>
2026-05-04 19:29:39 -0300

    idade

--- lacos.py
+++ lacos.py
@@ -1,1 +1,7 @@
-print("isso é facil")
+idade = input ("Digite idade:")
+if idade < 13:
+    print ("Criança")
+    elif idade < 18:
+        print ("Adolescente")
+        elif idade < 65:
+             print ("Adulto"):
